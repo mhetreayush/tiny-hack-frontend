9eb9341fb8d6ef78506a477073f40b98718a0082
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-01-18 14:40:50 +0530

    Start Scanning Button

--- src/componenets/NFCReader/index.jsx
+++ src/componenets/NFCReader/index.jsx
@@ -4,26 +4,24 @@ const NFCReader = ({children}) => {
   const [ndefReader, setNdefReader] = useState(undefined);
   const [supported, setSupported] = useState(true)
   const [serialNumber, setSerialNumber] = useState("SERIAL NUMBRE")
-
-  useEffect(() => {
-    async function startScanning(){
-      const ndef = new window.NDEFReader();
-      await ndef.scan();
-      ndef.addEventListener("reading", (card) => {
-        setSerialNumber(card.serialNumber) 
-      });
-    }
-
+  async function startScanning(){
     if (!("NDEFReader" in window)) {
       setSupported(false)
-    }else{
-      startScanning();
+      return 
     }
+    const ndef = new window.NDEFReader();
+    await ndef.scan();
+    ndef.addEventListener("reading", (card) => {
+      setSerialNumber(card.serialNumber) 
+    });
+  }
+
+  useEffect(() => {
   }, [])
 
   return (
     <div className="w-screen h-screen bg-[#3232fa]">
-    {supported &&<h1 className="text-xl text-[#fafafa]">{serialNumber}</h1>}
+    {supported &&<h1 className="text-xl text-[#fafafa]" onClick={startScanning}>{serialNumber}</h1>}
       {children} 
     </div>
   )
