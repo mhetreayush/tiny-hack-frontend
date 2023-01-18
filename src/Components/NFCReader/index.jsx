ead94361d5e0d3bc243bce7d81163f55f60aa523
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-01-18 16:52:08 +0530

    UserLogin

--- src/Components/NFCReader/index.jsx
+++ src/Components/NFCReader/index.jsx
@@ -1,9 +1,25 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+import Illustration from "./Illustration.svg"
+
+const LoginPage = ({ onClick, onChange, value }) => {
+ return (
+   <>
+    <input id="username" className="z-20 rounded-xl p-4 mb-24 w-[80%] h-12 bg-[#f0f0f0]" placeholder="Enter UserName" onChange={onChange} value={value}/>
+    <button className="z-20 bg-[#905DE6] mb-20 h-12 w-[80%] rounded-full drop-shadow-xl shadow-[#905DE632] font-bold text-[#ffffff]" onClick={onClick} >Next</button>
+    <img src={Illustration} className="absolute top-0 left-[50%] translate-x-[-50%] h-screen"/>
+   </>
+ ) 
+}
 
 const NFCReader = ({ children }) => {
   const [ndefReader, setNdefReader] = useState(undefined);
   const [supported, setSupported] = useState(true);
   const [serialNumber, setSerialNumber] = useState("TAP TO SCAN");
+  const [username, setUsername] = useState(null)
+  const [tempUsername, setTempUsername] = useState("")
+  const navigate = useNavigate()
+
   async function startScanning() {
     if (!("NDEFReader" in window)) {
       setSupported(false);
@@ -15,16 +31,16 @@ const NFCReader = ({ children }) => {
       setSerialNumber(card.serialNumber);
     });
   }
-
-  useEffect(() => {
-    window.addEventListener("load", (event) => {
-      startScanning();
-    });
-  }, []);
+ 
+  async function handleNext(){
+     setUsername(tempUsername)
+    await startScanning()
+    navigate("/user/" + tempUsername)
+  }
 
   return (
-    <div className="">
-      {supported && <h1 className="text-xl ">{serialNumber}</h1>}
+    <div className="flex flex-col justify-end items-center absolute top-0 left-0 w-screen h-screen bg-[#ffffff]">
+      {!username && <LoginPage onClick={handleNext} onChange={(e) => {setTempUsername(e.target.value)}} value={tempUsername}/>}
       {children}
     </div>
   );
